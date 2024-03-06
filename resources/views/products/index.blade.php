71697ef79cea0fcad0cf29da05d92d9787e59197
ThanMyintMaungThike <thanmyintmaungthike@gmail.com>
2024-03-06 22:23:48 +0630

    day03 assignment

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,41 @@
+@extends("layouts.app")
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Product List</title>
+</head>
+<body>
+    {{-- @dd($products); --}}
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Status</th>
+                <th scope="col">Price</th>
+                <th scope="col">Created At</th>
+                <th scope="col">Updated At</th>
+            </tr>
+        </thead>
+        @foreach ($products as $product)
+        <tbody>
+          <tr>
+            <th scope="row">{{ $product-> id }}</th>
+            <td>{{ $product-> name }}</td>
+            <td>{{ $product-> description }}</td>
+            <td>{{ $product-> status }}</td>
+            <td>{{ $product-> price }}</td>
+            <td>{{ $product-> created_at }}</td>
+            <td>{{ $product-> updated_at }}</td>
+          </tr>
+        </tbody>
+        @endforeach
+      </table>
+</body>
+</html>
+@endsection
